4ea907907dcedd31802c5cc11d22b32df3af09e0
Nate Klaiber <nate@theklaibers.com>
2021-09-10 12:31:34 -0400

    Extended nutrition plan response model

--- lib/core_nutrition/models/nutrition_plan.rb
+++ lib/core_nutrition/models/nutrition_plan.rb
@@ -26,11 +26,188 @@ module CoreNutrition
         @attributes['user_id']
       end
 
+      # Returns the Event ID
+      #
+      # @return [Integer]
+      def event_id
+        @attributes['event_id']
+      end
+
       # Returns the name
       #
       # @return [String]
       def name
         @attributes['name']
+      end
+
+      # Returns the event name
+      #
+      # @return [String]
+      def event_name
+        @attributes['event_name']
+      end
+
+      # Returns the goal name
+      #
+      # @return [String]
+      def goal_name
+        @attributes['goal_name']
+      end
+
+      # Returns the hydration preference value
+      #
+      # @return [Float]
+      def hydration_preference_percent_value
+        @attributes['hydration_preference_percent']
+      end
+
+      # Returns the hydration preference percent
+      #
+      # @return [Float]
+      def hydration_preference_percent
+        (self.hydration_preference_percent_value * 100.0)
+      end
+
+      # Returns the water preference value
+      #
+      # @return [Float]
+      def water_preference_percent_value
+        @attributes['water_preference_percent']
+      end
+
+      # Returns the water preference percent
+      #
+      # @return [Float]
+      def water_preference_percent
+        (self.water_preference_percent_value * 100.0)
+      end
+
+      # Returns the sweat rate value
+      #
+      # @return [String]
+      def sweat_rate_value
+        @attributes['sweat_rate_value']
+      end
+
+      # Returns the sweat rate unit attributes
+      #
+      # @return [Hash]
+      def sweat_rate_unit_attributes
+        @attributes['sweat_rate_unit']
+      end
+
+      # Returns the sweat rate unit
+      #
+      # @return [CoreNutrition::Models::Unit]
+      def sweat_rate_unit
+        begin
+          CoreNutrition::Models::Unit.new(self.sweat_rate_unit_attributes)
+        rescue ArgumentError
+          nil
+        end
+      end
+
+      # Returns true if there is a sweat rate unit
+      #
+      # @return [Boolean]
+      def sweat_rate_unit?
+        !self.sweat_rate_unit.nil?
+      end
+
+      # Returns the weight value
+      #
+      # @return [String]
+      def weight_value
+        @attributes['weight_value']
+      end
+
+      # Returns the weight unit attributes
+      #
+      # @return [Hash]
+      def weight_unit_attributes
+        @attributes['weight_unit']
+      end
+
+      # Returns the weight unit
+      #
+      # @return [CoreNutrition::Models::Unit]
+      def weight_unit
+        begin
+          CoreNutrition::Models::Unit.new(self.weight_unit_attributes)
+        rescue ArgumentError
+          nil
+        end
+      end
+
+      # Returns true if there is a weight unit
+      #
+      # @return [Boolean]
+      def weight_unit?
+        !self.weight_unit.nil?
+      end
+
+      # Returns the temperature value
+      #
+      # @return [String]
+      def temperature_value
+        @attributes['temperature_value']
+      end
+
+      # Returns the temperature unit attributes
+      #
+      # @return [Hash]
+      def temperature_unit_attributes
+        @attributes['temperature_unit']
+      end
+
+      # Returns the temperature unit
+      #
+      # @return [CoreNutrition::Models::Unit]
+      def temperature_unit
+        begin
+          CoreNutrition::Models::Unit.new(self.temperature_unit_attributes)
+        rescue ArgumentError
+          nil
+        end
+      end
+
+      # Returns true if there is a temeprature unit
+      #
+      # @return [Boolean]
+      def temperature_unit?
+        !self.temperature_unit.nil?
+      end
+
+      # Returns true if restricting carb intake
+      #
+      # @return [Boolean]
+      def is_restricting_carb_intake
+        @attributes['is_restricting_carb_intake']
+      end
+      alias restricting_carb_intake? is_restricting_carb_intake
+      alias is_restricting_carb_intabke? is_restricting_carb_intake
+
+      # Returns true if has weight loss focus
+      #
+      # @return [Boolean]
+      def has_weight_loss_focus
+        @attributes['has_weight_loss_focus']
+      end
+      alias has_weight_loss_focus? has_weight_loss_focus
+      alias weight_loss_focus? has_weight_loss_focus
+
+      # Returns the carbohydrate training frequency value
+      #
+      # @return [String]
+      def carbohydrate_training_frequency_category_value
+        @attributes['carbohydrate_training_frequency_category_value']
+      end
+
+      # Returns the caffeine sensitivity category name
+      #
+      # @return [String]
+      def caffeine_sensitivity_category_name
+        @attributes['caffeine_sensitivity_category_name']
       end
 
       # Returns the created at timestamp
