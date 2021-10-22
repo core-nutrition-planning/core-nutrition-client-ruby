b82c09f4d2444dffe90a1e25dd9d44cb4932f6a4
Nate Klaiber <nate@theklaibers.com>
2021-10-22 15:00:41 -0400

    Auto paginate the timezones

--- lib/core_nutrition/models/timezones.rb
+++ lib/core_nutrition/models/timezones.rb
@@ -14,19 +14,66 @@ module CoreNutrition
       #
       # @return [CoreNutrition::Models::]
       def self.list(params={})
-        request = CoreNutrition::Requests::Timezones.list(params)
+        default_params = {
+          :page          => 1,
+          :per_page      => 250,
+          :auto_paginate => (params.delete(:auto_paginate) || false)
+        }
 
-        klass_attributes = []
+        request_params = default_params.merge!(params)
+
+        auto_paginate = request_params.delete(:auto_paginate)
+
+        request = CoreNutrition::Requests::Timezones.list(request_params)
+
+        request_records = []
 
         request.on(:success) do |resp|
-          klass_attributes = resp.data_attributes
+          if resp.status == 200
+            response_body  = resp.body
+            response_links = response_body.fetch('links', [])
+            response_data  = response_body.fetch('data', [])
+
+            request_records.concat(response_data)
+
+            links = CoreNutrition::Models::Links.new(response_links)
+
+            next_link = links.find_by_rel('next')
+
+            if auto_paginate && next_link
+              loop do
+                sub_request = CoreNutrition::Client.get(next_link.url_for)
+
+                sub_response_body = []
+
+                sub_request.on(:success) do |sub_resp|
+
+                  sub_response_body = sub_resp.body
+                  sub_response_data = sub_response_body.fetch('data', [])
+                  sub_response_links = sub_response_body.fetch('links', [])
+
+                  request_records.concat(sub_response_data)
+
+                  links = CoreNutrition::Models::Links.new(sub_response_links)
+
+                  next_link = links.find_by_rel('next')
+                end
+
+                break if next_link.nil?
+              end
+            end
+          else
+            CoreNutrition::Client.configuration.logger.error { "Failure in paginating" % [resp.status] }
+          end
+
+          return CoreNutrition::Models::Timezones.new(request_records)
         end
 
         request.on(:failure) do |resp|
           CoreNutrition::Client.configuration.logger.error { ("Failure retrieving the records: %s" % [resp.status]) }
         end
 
-        self.new(klass_attributes)
+        self.new(request_records)
       end
 
       # Retrieve all options
